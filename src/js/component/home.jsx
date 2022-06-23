8166e5c9c7d81ff329d45e5e82429dcbe3c21087
jorge06br <jorgebaca06@gmail.com>
2022-06-23 04:47:04 +0000

    [PENDIENTE REVISAR BUCLE INFINITO POR TERNARIOS]

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
 
-//include images into your bundle
+// include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
-//create your first component
+// create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+    const [Color, setColor] = useState(25);
+    const [selector, setselector] = useState(false);
+    return (
+        <div className="container bg-dark vh-100" id="semaforo">
+            <span onMouseEnter={
+                    () => {
+                        setselector(true);
+                    }
+                }
+                className={
+                    `position-absolute top-0 start-50 translate-middle-x my-5 bg-success bg-opacity-${
+                        selector == true ? (onmouseleave =() => {
+                            setselector(false),
+                            setColor(25);
+                        }) : setColor(100)
+                    }`
+            }></span>
+        <span onMouseEnter={
+                () => setColor(100)
+            }
+            className={
+                `position-absolute top-50 start-50 translate-middle bg-warning bg-opacity-${Color}`
+        }></span>
+        <span onMouseEnter={
+                () => setColor(100)
+            }
+            className={
+                `position-absolute bottom-0 start-50 translate-middle-x my-5 bg-danger bg-opacity-${Color}`
+        }></span>
+    </div>
+    );
 };
 
 export default Home;
